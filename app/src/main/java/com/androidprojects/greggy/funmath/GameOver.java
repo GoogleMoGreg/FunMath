86b7b1610a5e80a8d4e07f2a415e7d2d4d240872
greggy <greg.funtera@gmail.com>
2017-04-12 14:42:11 +0800

    Added Buttons in GameOver Layout

--- app/src/main/java/com/androidprojects/greggy/funmath/GameOver.java
+++ app/src/main/java/com/androidprojects/greggy/funmath/GameOver.java
@@ -1,14 +1,18 @@
 package com.androidprojects.greggy.funmath;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class GameOver extends AppCompatActivity {
+public class GameOver extends AppCompatActivity implements View.OnClickListener {
 
     TextView tv_score, tv_highscore;
+    Button btn_y,btn_n;
 
     String TAG_BUNDLE_SCORE = "score";
     String TAG_BUNDLE_PK = "pk";
@@ -24,6 +28,10 @@ public class GameOver extends AppCompatActivity {
         dbHelper = new DBHelper(this);
         tv_score = (TextView) findViewById(R.id.tv_gameoverScore);
         tv_highscore = (TextView) findViewById(R.id.tv_highScore);
+        btn_y=(Button)findViewById(R.id.btn_tryagain_y);
+        btn_y.setOnClickListener(this);
+        btn_n=(Button)findViewById(R.id.btn_tryagain_n);
+        btn_n.setOnClickListener(this);
 
         Bundle bundle = getIntent().getExtras();
         int score = bundle.getInt(TAG_BUNDLE_SCORE);
@@ -34,7 +42,20 @@ public class GameOver extends AppCompatActivity {
         tv_highscore.setText("Highscore is " + String.valueOf(highScore));
         IdentifyScore(score,highScore);
 
+    }
 
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+
+            case R.id.btn_tryagain_y:
+                Log.d(TAG_MESSAGE,"pressed yes button...");
+                break;
+            case R.id.btn_tryagain_n:
+                Log.d(TAG_MESSAGE,"pressed no button...");
+                break;
+        }
     }
 
     private void IdentifyScore(int score, int highscore) {

20339345bcd0794f26e61001cff4bfa6ba310d99
greggy <greg.funtera@gmail.com>
2017-04-27 12:19:16 +0800

    Finished Game Over Layout

--- app/src/main/java/com/androidprojects/greggy/funmath/GameOver.java
+++ app/src/main/java/com/androidprojects/greggy/funmath/GameOver.java
@@ -10,11 +10,13 @@ import android.widget.TextView;
 
 public class GameOver extends AppCompatActivity implements View.OnClickListener {
 
-    TextView tv_score, tv_highscore;
-    Button btn_y,btn_n;
+    Button btn_y,btn_n,btn_scoreis,btn_highscore;
+    TextView tv_gamecategory;
 
     String TAG_BUNDLE_SCORE = "score";
     String TAG_BUNDLE_PK = "pk";
+
+    int pk;
 
     String TAG_MESSAGE = "DEBUG_MESSAGE";
 
@@ -25,8 +27,9 @@ public class GameOver extends AppCompatActivity implements View.OnClickListener 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
         dbHelper = new DBHelper(this);
-        tv_score = (TextView) findViewById(R.id.tv_gameoverScore);
-        tv_highscore = (TextView) findViewById(R.id.tv_highScore);
+        tv_gamecategory=(TextView) findViewById(R.id.tv_gamecategory);
+        btn_scoreis=(Button)findViewById(R.id.imgbtn_score);
+        btn_highscore=(Button)findViewById(R.id.imgbtn_bestscore);
         btn_y=(Button)findViewById(R.id.btn_tryagain_y);
         btn_y.setOnClickListener(this);
         btn_n=(Button)findViewById(R.id.btn_tryagain_n);
@@ -34,11 +37,13 @@ public class GameOver extends AppCompatActivity implements View.OnClickListener 
 
         Bundle bundle = getIntent().getExtras();
         int score = bundle.getInt(TAG_BUNDLE_SCORE);
-        int pk = bundle.getInt(TAG_BUNDLE_PK);
+        pk = bundle.getInt(TAG_BUNDLE_PK);
+        SetCategory(pk);
+        Log.d(TAG_MESSAGE,"pk is "+pk);
         Log.d(TAG_MESSAGE, "score is :" + score);
         int highScore = dbHelper.ViewHighScore(pk);
-        tv_score.setText("Your score is " + String.valueOf(score));
-        tv_highscore.setText("Highscore is " + String.valueOf(highScore));
+        btn_scoreis.setText(String.valueOf(score));
+        btn_highscore.setText(String.valueOf(highScore));
         IdentifyScore(score,highScore);
 
     }
@@ -50,9 +55,12 @@ public class GameOver extends AppCompatActivity implements View.OnClickListener 
 
             case R.id.btn_tryagain_y:
                 Log.d(TAG_MESSAGE,"pressed yes button...");
+                Log.d(TAG_MESSAGE,"pk is "+pk);
+                GoToActivity(pk);
                 break;
             case R.id.btn_tryagain_n:
                 Log.d(TAG_MESSAGE,"pressed no button...");
+                CloseAcitivty();
                 break;
         }
     }
@@ -66,6 +74,60 @@ public class GameOver extends AppCompatActivity implements View.OnClickListener 
 
         }
 
+    }
+
+    private void SetCategory(int pk){
+
+        switch (pk){
+
+            case 1:
+                tv_gamecategory.setText("a d d i t i o n");
+                break;
+            case 2:
+                tv_gamecategory.setText("s u b t r a c t i o n");
+                break;
+            case 3:
+                tv_gamecategory.setText("m u  l t i p l i c a t i o n");
+                break;
+        }
+    }
+
+    private void GoToActivity(int pk){
+
+        switch (pk){
+
+            case 1:
+                OpenAddition();
+                break;
+            case 2:
+                OpenSubtraction();
+                break;
+            case 3:
+                OpenMultiplication();
+                break;
+        }
+    }
+
+    private void OpenAddition(){
+        Intent add = new Intent(this,Addition.class);
+        this.finish();
+        startActivity(add);
+    }
+
+    private void OpenSubtraction(){
+        Intent sub = new Intent(this,Subtraction.class);
+        this.finish();
+        startActivity(sub);
+    }
+
+    private void OpenMultiplication(){
+        Intent mul = new Intent(this,Multiplication.class);
+        this.finish();
+        startActivity(mul);
+    }
+
+    private void CloseAcitivty(){
+        this.finish();
     }
 
     @Override
